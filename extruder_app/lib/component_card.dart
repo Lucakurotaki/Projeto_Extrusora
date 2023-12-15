4d2a0acccc6f883d86b3e1b9a656255cb19e4ec2
Luca Kurotaki <lucakurotaki12@gmail.com>
2023-12-15 20:41:38 -0300

    final version

--- extruder_app/lib/component_card.dart
+++ extruder_app/lib/component_card.dart
@@ -1,40 +1,61 @@
-import 'package:extruder_app/component_iconbutton.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ComponentCard extends StatefulWidget {
-  final component;
-  final componentType;
-  final lable;
-  final unit;
+  final String component;
+  final String componentType;
+  final String lable;
+  final String unit;
 
-  const ComponentCard(this.component, this.componentType, this.lable, this.unit, {super.key});
+  const ComponentCard(this.component, this.componentType, this.lable, this.unit,
+      {super.key});
 
   @override
   State<ComponentCard> createState() => _ComponentCardState();
 }
 
 class _ComponentCardState extends State<ComponentCard> {
-  
   num sensor = 0;
   num control = 0;
 
-  bool increment = false;
-  bool decrement = false;
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
 
   @override
   void initState() {
     super.initState();
     _extruderDataFetch();
+
+    _focusNode.addListener(() {
+      if(!_focusNode.hasFocus){
+        _focusNode.unfocus();
+        setState(() {
+          textfield = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  DatabaseReference dBFetch() {
+    final componentType = widget.componentType;
+    final component = widget.component;
+
+    DatabaseReference componentRef =
+        FirebaseDatabase.instance.ref('$componentType/controller/$component');
+
+    return componentRef;
   }
 
   void _extruderDataFetch() {
-    final componentType = widget.componentType;
-
-    DatabaseReference dBRef =
-      FirebaseDatabase.instance.ref('$componentType/controller/');
-
-    final componentRef = dBRef.child('${widget.component}');
+    final componentRef = dBFetch();
 
     componentRef.onValue.listen((DatabaseEvent event) {
       final data = event.snapshot.value as Map;
@@ -42,16 +63,22 @@ class _ComponentCardState extends State<ComponentCard> {
       setState(() {
         sensor = data['sensor'] as num;
         control = data['control'] as num;
-        increment = data['increment'] as bool;
-        decrement = data['decrement'] as bool;
       });
+
+      _controller.text = '$control';
     });
   }
 
+  void _updateDB(num value) async {
+    await dBFetch().update({
+      "control": value,
+    });
+  }
+
+  bool textfield = false;
+
   @override
   Widget build(BuildContext context) {
-    final component = widget.component;
-    final componentType = widget.componentType;
     final lable = widget.lable;
     final unit = widget.unit;
 
@@ -62,14 +89,59 @@ class _ComponentCardState extends State<ComponentCard> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Column(
-                children: [
-                  Text('$sensor $unit', style: TextStyle(fontSize: 18, color: Colors.redAccent[700]),),
-                  Text('$control $unit', style: TextStyle(fontSize: 18, color: Colors.green[900]),),
-                ],
+              Text(
+                '$sensor $unit',
+                style: TextStyle(fontSize: 26, color: Colors.green[900]),
               ),
-              ComponentIconButton(component, componentType, 'increment', increment, Icons.arrow_circle_up_outlined),
-              ComponentIconButton(component, componentType, 'decrement', decrement, Icons.arrow_circle_down_outlined),
+              textfield == true
+                  ? SizedBox(
+                      width: 60,
+                      height: 30,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                        autofocus: true,
+                        focusNode: _focusNode,
+                        controller: _controller,
+                        style: TextStyle(
+                            fontSize: 26, color: Colors.redAccent[700]),
+                        textAlign: TextAlign.center,
+                        onSubmitted: (value) {
+                          num numValue = num.parse(value);
+                          _updateDB(numValue);
+                          setState(() {
+                            textfield = false;
+                          });
+                        },
+                      ),
+                    )
+                  : TextButton(
+                      onPressed: () {
+                        textfield = true;
+                        setState(() {});
+                      },
+                      child: Text(
+                        '$control $unit',
+                        style: TextStyle(
+                            fontSize: 26, color: Colors.redAccent[700]),
+                      ),
+                    ),
+              IconButton(
+                onPressed: () {
+                  num value = ++control;
+                  _updateDB(value);
+                },
+                icon: const Icon(Icons.arrow_circle_up_outlined),
+                iconSize: 35,
+              ),
+              IconButton(
+                onPressed: () {
+                  num value = --control;
+                  _updateDB(value);
+                },
+                icon: const Icon(Icons.arrow_circle_down_outlined),
+                iconSize: 35,
+              ),
             ],
           ),
           const Text('')
--- extruder_app/lib/main.dart
+++ extruder_app/lib/main.dart
@@ -45,18 +45,39 @@ class _HomePageState extends State<HomePage> {
   bool heaterSwitch = false;
   bool motorSwitch = false;
 
+  dynamic temperature = {};
+  Map preset = {};
+
+  List presetKeys = [];
+
+  late TextEditingController controller;
+
+  String name = '';
+
   @override
   void initState() {
     super.initState();
     _extruderDataFetch();
+
+    controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
   }
 
   void _extruderDataFetch() {
     dBRef.onValue.listen((DatabaseEvent event) {
       final data = event.snapshot.value as Map;
 
-      final temperature = data['temperature'];
+      temperature = data['temperature'];
       final velocity = data['velocity'];
+      preset = data['preset'] as Map;
+
+      presetKeys = preset.keys.toList();
 
       setState(() {
         heaterSwitch = temperature['button'] as bool;
@@ -65,7 +86,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void _setDB(String path, String key, value) async {
+  void _updateDB(String path, String key, value) async {
     await dBRef.child(path).update({
       key: value,
     });
@@ -74,72 +95,219 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.greenAccent[700],
-          title: Text(widget.title),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent[400],
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Colors.deepPurple),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Expanded(
-                  flex: 2,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Switch(
-                                value: heaterSwitch,
-                                onChanged: ((value) {
-                                  setState(() {
-                                    heaterSwitch = value;
-                                    _setDB(
-                                        "temperature", "button", heaterSwitch);
-                                  });
-                                })),
-                            const Text('Aquecimento')
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Switch(
-                                value: motorSwitch,
-                                onChanged: ((value) {
-                                  setState(() {
-                                    motorSwitch = value;
-                                    _setDB("velocity", "button", motorSwitch);
-                                  });
-                                })),
-                            const Text('Motor')
-                          ],
-                        ),
-                      ])),
-              const Divider(),
-              const Expanded(
-                flex: 7,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ComponentCard('heater01', 'temperature','Aquecedor 1', '°C'),
-                    ComponentCard('heater02', 'temperature','Aquecedor 2', '°C'),
-                    ComponentCard('heater03', 'temperature','Aquecedor 3', '°C'),
-                    ComponentCard('heater04', 'temperature','Aquecedor 4', '°C'),
-                  ],
-                ),
-              ),
-              const Divider(),
-              const Expanded(
-                  flex: 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ComponentCard('motor', 'velocity','Velocidade', 'rpm'),
-                    ],
-                  )),
-            ],
+      ),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.greenAccent[400],
+            ),
+            child: const Text(
+              'Presets',
+              style: TextStyle(fontSize: 25, color: Colors.deepPurple),
+              textAlign: TextAlign.center,
+            ),
           ),
-        ));
+          ListView.builder(
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              itemCount: presetKeys.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                    key: Key(presetKeys[index]),
+                    confirmDismiss: (direction) async {
+                      return await _deleteConfirmationDialog(context);
+                    },
+                    onDismissed: (direction) { 
+
+                      _updateDB('preset', presetKeys[index], null);
+                    },
+                    child: ListTile(
+                      title: Text(presetKeys[index]),
+                      onTap: () {
+                        Map selectedPreset = preset[presetKeys[index]] as Map;
+                        for (var key in selectedPreset.keys) {
+                          _updateDB('temperature/controller/$key', 'control',
+                              selectedPreset[key]);
+                        }
+                        Navigator.pop(context);
+                      },
+                    ));
+              })
+        ],
+      )),
+      body: Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Switch(
+                    value: heaterSwitch,
+                    onChanged: ((value) {
+                      setState(() {
+                        heaterSwitch = value;
+                        _updateDB("temperature", "button", heaterSwitch);
+                      });
+                    })),
+                const Text('Aquecimento')
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Switch(
+                    value: motorSwitch,
+                    onChanged: ((value) {
+                      setState(() {
+                        motorSwitch = value;
+                        _updateDB("velocity", "button", motorSwitch);
+                      });
+                    })),
+                const Text('Motor')
+              ],
+            ),
+          ]),
+          const Divider(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  heaterSwitch == true
+                      ? const Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ComponentCard(
+                                'heater01', 'temperature', 'Aquecedor 1', '°C'),
+                            ComponentCard(
+                                'heater02', 'temperature', 'Aquecedor 2', '°C'),
+                            ComponentCard(
+                                'heater03', 'temperature', 'Aquecedor 3', '°C'),
+                            ComponentCard(
+                                'heater04', 'temperature', 'Aquecedor 4', '°C'),
+                          ],
+                        )
+                      : const SizedBox(),
+                  const Divider(),
+                  motorSwitch == true
+                      ? const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ComponentCard(
+                                'motor', 'velocity', 'Velocidade', 'rpm'),
+                          ],
+                        )
+                      : const SizedBox(),
+                  const Divider(),
+                  heaterSwitch == true
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: TextButton(
+                            onPressed: () async {
+                              final name = await saveDialog();
+                              if (name == null || name.isEmpty) return;
+                              setState(() {
+                                this.name = name;
+                              });
+                            },
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.greenAccent[400],
+                                fixedSize: const Size.fromWidth(100)),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [Icon(Icons.save), Text('Salvar')],
+                            ),
+                          ),
+                        )
+                      : const SizedBox()
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<String?> saveDialog() => showDialog<String>(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: const Text(
+              "Salvar configuração como: ",
+              style: TextStyle(fontSize: 20),
+            ),
+            content: TextField(
+              autofocus: true,
+              decoration: const InputDecoration(
+                hintText: "Nome",
+              ),
+              controller: controller,
+            ),
+            actions: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                TextButton(onPressed: cancel, child: const Text('Cancelar')),
+                TextButton(onPressed: submit, child: const Text('Salvar')),
+              ])
+            ],
+          ));
+
+  void submit() {
+    String presetName = controller.text;
+
+    final tempController = temperature['controller'];
+    num heater1 = tempController['heater01']['control'];
+    num heater2 = tempController['heater02']['control'];
+    num heater3 = tempController['heater03']['control'];
+    num heater4 = tempController['heater04']['control'];
+
+    dynamic presetTemps = {
+      "heater01": heater1,
+      "heater02": heater2,
+      "heater03": heater3,
+      "heater04": heater4
+    };
+
+    _updateDB("preset", presetName, presetTemps);
+    Navigator.of(context).pop(controller.text);
+  }
+
+  void cancel() {
+    Navigator.of(context).pop();
+  }
+
+  Future<bool> _deleteConfirmationDialog(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmação de Exclusão'),
+          content: const Text('Deseja realmente excluir o item?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            TextButton(
+              child: const Text('Excluir'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
